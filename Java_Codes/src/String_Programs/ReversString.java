b395a336aff48a798c417b3f16d896521fec095d
mpavansai007 <pavansai20092000@gmail.com>
2026-02-05 20:47:50 +0530

    add new file content

--- Java_Codes/src/String_Programs/ReversString.java
+++ Java_Codes/src/String_Programs/ReversString.java
@@ -1,5 +1,12 @@
 package String_Programs;
 
 public class ReversString {
+	public static void main(String []arg) {
+	String str = "Pavan";
+	 String rev = "";
+	for(int i = str.length() - 1; i >= 0; i--) {
+		rev =rev+str.charAt(i);
+	}
+	System.out.println(rev);
 
-}
+}}
